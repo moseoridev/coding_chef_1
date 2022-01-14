94f4b750bb52ac4990754092212b8e360a632754
moseoridev <sjssjs1344@gmail.com>
2022-01-15 01:23:18 +0900

    Navogator 사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,54 @@
-import 'package:coding_chef_1/spiderman.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Bar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
+      home: FirstPage(),
+      title: 'Test App',
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyPage extends StatelessWidget {
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Bar Icon Menu'),
-        centerTitle: true,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('shopping cart button clicked');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('search button clicked');
-            },
-          )
-        ],
+        title: Text('First Page'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/1.jpg'),
-              ),
-              accountName: Text('Moseori'),
-              accountEmail: Text('account@example.com'),
-              onDetailsPressed: () => {print('arrow clicked')},
-              decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40),
-                ),
-              ),
-              otherAccountsPictures: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/2.jpg'),
-                )
-              ],
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('Home'),
-              onTap: () => print('Home clicked'),
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Setting'),
-              onTap: () => print('Setting clicked'),
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('QnA'),
-              onTap: () => print('QnA clicked'),
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
-      ),
+      body: Center(
+          child: ElevatedButton(
+        child: Text('Go to the 2nd page'),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SecondPage()));
+        },
+      )),
     );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Second Page'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          child: Text('Go to the 1st page'),
+          onPressed: () => Navigator.pop(context),
+        )));
   }
 }
