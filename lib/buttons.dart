4364cb000a859fe5c8d0c3537d571cf35b7d3bc5
moseoridev <sjssjs1344@gmail.com>
2022-01-16 02:48:53 +0900

    snackbar 함수 구현

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class MyButtons extends StatelessWidget {
   const MyButtons({Key? key}) : super(key: key);
+  void _show_snackbar(ctx, msg) {
+    ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(content: Text(msg)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +17,16 @@ class MyButtons extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton(
-              onPressed: () => ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text('text button'))),
+              onPressed: () => _show_snackbar(context, 'pressed!'),
               child: Text(
                 'Text Button',
                 style: TextStyle(fontSize: 20),
               ),
-              style: TextButton.styleFrom(primary: Colors.red),
+              onLongPress: () => _show_snackbar(context, 'long pressed!'),
+              style: TextButton.styleFrom(
+                primary: Colors.red,
+                // backgroundColor: Colors.blue,
+              ),
             )
           ],
         ),
