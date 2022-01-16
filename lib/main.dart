cfe0f4521f50595b795fb0e18597880cd002a7b5
moseoridev <sjssjs1344@gmail.com>
2022-01-17 01:58:07 +0900

    title 오타 수정

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Named routes Demo',
+      title: 'Named Routes Demo',
       initialRoute: '/',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
